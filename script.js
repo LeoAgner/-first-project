0f723b9e9a1d02691742e10962c3e0421ce0ad31
Leo Agner <leoagner12@gmail.com>
2023-01-21 08:22:46 -0300

    update in alert dayExists

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function add() {
     const dayExists = nlwSetup.dayExists(today)
 
     if(dayExists) {
-        alert('Dia já incluso 🎈')
+        alert("Dia já incluso🎈")
         return
     }
 
